a8f5a82c6f229b2d4d4fea81da6a42b8afabac74
Geoff Jamieson <geoffjamieson@Geoffs-MacBook-Pro.local>
2022-10-20 13:23:54 -0400

    added dependencies & modded hardhat config

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
-# Hey 👋 I'm a new hardhat template!
+# Hardhat Upgrades
 
-I work with hardhat a lot. This is my preferred starting point when working on a project. I created this basic template
-to save me some time so I don't have to set things up from scratch every time! 
+reference @ https://forum.openzeppelin.com/t/openzeppelin-upgrades-step-by-step-tutorial-for-hardhat/3580/1
 
-If you'd like to use it go right ahead! It's pretty plug & play 🙂
+### This will be a demo of upgrading the Box.sol contract to BoxV2.sol
+
+#### How does this work? <br>
+
+In order to upgrade Box -> BoxV2 ... <br>
+We need to have a Proxy that points to the specific implementation contract we want to use. <br>
+So we will need to point to BoxV2 instead of Box <br>
+
+<pre>
+Proxy     Box <br>
+       -> BoxV2
+</pre>
+
+#### So how can we do this?
+
+1. Deploy a Proxy
+   - Manually
+   - Using hardhat deploy plugin (https://github.com/wighawag/hardhat-deploy#deploying-and-upgrading-proxies)
+   - Using Open Zeppelin hardhat upgrades (https://docs.openzeppelin.com/upgrades-plugins/1.x/api-hardhat-upgrades)
+
+
+🟢  This demo will be using the hardhat-deploy method 🟢
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,12 @@
-const { task } = require("hardhat/config")
-require("@nomicfoundation/hardhat-toolbox")
-require("hardhat-contract-sizer")
-require("hardhat-deploy")
-require("dotenv").config()
+const { task } = require("hardhat/config");
+require("@nomicfoundation/hardhat-toolbox");
+require("@openzeppelin/hardhat-upgrades");
+require("hardhat-contract-sizer");
+require("hardhat-deploy");
+require("dotenv").config();
 
 /* Tasks */
-require("./tasks/accounts")
+require("./tasks/accounts");
 
 /* 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
@@ -15,16 +16,16 @@ const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 */
 
 module.exports = {
-    defaultNetwork: "hardhat",
-    networks: {
-        hardhat: {
-            chainId: 31337,
-            blockConfirmations: 1,
-        },
-        localhost: {
-            chainId: 31337,
-        },
-        /* 
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+    /* 
         rinkeby: {
             chainId: 4,
             saveDeployments: true,
@@ -40,33 +41,33 @@ module.exports = {
             accounts: [PRIVATE_KEY],
         }, 
         */
+  },
+  solidity: {
+    compilers: [{ version: "0.8.17" }, { version: "0.7.0" }],
+  },
+  etherscan: {
+    // apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    // coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+  contractSizer: {
+    runOnCompile: false,
+    only: [""],
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
     },
-    solidity: {
-        compilers: [{ version: "0.8.17" }],
+    randomPerson: {
+      default: 1,
     },
-    etherscan: {
-        // apiKey: ETHERSCAN_API_KEY,
-    },
-    gasReporter: {
-        enabled: true,
-        outputFile: "gas-report.txt",
-        noColors: true,
-        currency: "USD",
-        // coinmarketcap: COINMARKETCAP_API_KEY,
-    },
-    contractSizer: {
-        runOnCompile: false,
-        only: [""],
-    },
-    namedAccounts: {
-        deployer: {
-            default: 0,
-        },
-        randomPerson: {
-            default: 1,
-        },
-    },
-    mocha: {
-        timeout: 100000,
-    },
-}
+  },
+  mocha: {
+    timeout: 100000,
+  },
+};
